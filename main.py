924ef488baa01707f0db9d7de739510d3f22c035
MehMessGo <pmdmix1@gmail.com>
2020-05-05 18:12:14 +0700

    Refactoring

--- main.py
+++ main.py
@@ -6,85 +6,100 @@ import cv2
 import sys
 
 
-def aruco_get_markers(image):
-    dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
-    markerImage = np.zeros((200, 200), dtype=np.uint8)
-    markerImage = cv2.aruco.drawMarker(dictionary, 23, 200, markerImage, 1)
+def alpha_blending(background, foreground, alpha=None):
+    """Alpha blending.
 
-    parameters = cv2.aruco.DetectorParameters_create()
+    Наложение foreground на background.
+    Если alpha = None, то наложение происходит с ипсользованием альфа-канала foreground
 
-    markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(image, dictionary, parameters=parameters)
-    return markerCorners, markerIds, rejectedCandidates
+    Возвращает image - результат наложения
+    """
+    if alpha is None and foreground.shape[2] == 4:
+        alpha = foreground[:, :, 3] // 255
+
+    image = np.copy(background)
+    image[:, :, 0] = image[:, :, 0] * (1 - alpha) + foreground[:, :, 0] * alpha
+    image[:, :, 1] = image[:, :, 1] * (1 - alpha) + foreground[:, :, 1] * alpha
+    image[:, :, 2] = image[:, :, 2] * (1 - alpha) + foreground[:, :, 2] * alpha
+    return image
+
+
+def read_input(input_type, source):
+    """Read input
+
+    Считать входное изображение
+
+    Аргументы:
+        input_type: '1'/'2' - изображение/видео
+        source: для файла указать путь: 'image.jpg' или 'video.mp4.
+            для камеры: 0-9 - номер камеры или '192.168.0.xxx:port' - для ip камеры
+
+    Возвращает image - считанное изображение
+    """
+
+    flag, photo_bgr = None, None
+    if input_type == '1':
+        photo_bgr = source
+    if input_type == '2':
+        flag, photo_bgr = source.read()
+    return modules.rescale(photo_bgr, 500)
 
 
 def main():
     input_type, source = modules.get_input()
-    photo_bgr = None
+    photo_bgr = read_input(input_type, source)
 
     img_paste = cv2.imread('input.png', cv2.IMREAD_UNCHANGED)
     h, w = img_paste.shape[:2]
-    pts1 = np.float32([[0, 0], [w, 0], [w, h], [0, h]])
+    points_paste = np.float32([[0, 0], [w, 0], [w, h], [0, h]])
 
-    while True:
-        if input_type == '1':
-            photo_bgr = modules.rescale(source, 500)
-        if input_type == '2':
-            flag, photo_bgr = source.read()
-            if cv2.waitKey(1) == 27:  # ESC
-                break
+    # Стартова модель - квадрат - описывающий acuro маркер в пространстве
+    model_points = np.array([[0, 0, 0], [100, 0, 0], [100, 100, 0], [0, 100, 0]], dtype=np.float32)
+    # Точки которые должны быть перемещены в новую позицую
+    points3d = np.array([(0.0, 50.0, -50.0), (0.0, 50.0, -100.0), (100.0, 50.0, -100.0), (100.0, 50.0, -50.0)])
 
-        markerCorners, markerIds, rejectedCandidates = aruco_get_markers(photo_bgr)
-        cv2.aruco.drawDetectedMarkers(photo_bgr, markerCorners, markerIds)
+    camera_matrix = np.array([[photo_bgr.shape[1], 0, photo_bgr.shape[1] / 2],
+                              [0, photo_bgr.shape[1], photo_bgr.shape[0] / 2],
+                              [0, 0, 1]], dtype=np.float32)
 
-        if len(markerCorners) > 0:
-            for corners in markerCorners:
-                pts2 = corners
-                matrix = cv2.getPerspectiveTransform(pts1, pts2)
-                result = cv2.warpPerspective(img_paste, matrix, photo_bgr.shape[:2][::-1])
+    dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+    parameters = cv2.aruco.DetectorParameters_create()
 
-                alpha = result[:, :, 3] // 255
-                photo_bgr[:, :, 0] = photo_bgr[:, :, 0] * (1 - alpha) + result[:, :, 0] * alpha
-                photo_bgr[:, :, 1] = photo_bgr[:, :, 1] * (1 - alpha) + result[:, :, 1] * alpha
-                photo_bgr[:, :, 2] = photo_bgr[:, :, 2] * (1 - alpha) + result[:, :, 2] * alpha
+    while cv2.waitKey(1) != 27:  # пока не нажат #ESC
+        photo_bgr = read_input(input_type, source)
 
-                ###############################################################################
-                model_points = np.array([[0, 0, 0],
-                                         [100, 0, 0],
-                                         [100, 100, 0],
-                                         [0, 100, 0]], dtype=np.float32)
+        markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(photo_bgr, dictionary,
+                                                                               parameters=parameters)
 
-                camera_matrix = np.array([[photo_bgr.shape[1], 0, photo_bgr.shape[1] / 2],
-                                          [0, photo_bgr.shape[1], photo_bgr.shape[0] / 2],
-                                          [0, 0, 1]], dtype=np.float32)
+        for corners in markerCorners:
+            matrix = cv2.getPerspectiveTransform(points_paste, corners)
+            result = cv2.warpPerspective(img_paste, matrix, photo_bgr.shape[:2][::-1])
 
-                dist_coeffs = np.zeros((4, 1), dtype=np.float32)  # Assuming no lens distortion
+            photo_bgr = alpha_blending(photo_bgr, result)
 
-                success, rotation_vector, translation_vector = cv2.solvePnP(model_points, pts2, camera_matrix,
-                                                                            dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
+            dist_coeffs = np.zeros((4, 1), dtype=np.float32)
 
-                points3d = np.array([(0.0, 50.0, -50.0),
-                                     (0.0, 50.0, -100.0),
-                                     (100.0, 50.0, -100.0),
-                                     (100.0, 50.0, -50.0)])
-                points2D, _ = cv2.projectPoints(points3d,
-                                                rotation_vector,
-                                                translation_vector, camera_matrix, dist_coeffs)
+            _, rotation_vector, translation_vector = cv2.solvePnP(model_points, corners, camera_matrix,
+                                                                  dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
 
-                ap = (int(points2D[3][0][0]), int(points2D[3][0][1]))
-                bp = (int(points2D[0][0][0]), int(points2D[0][0][1]))
-                cv2.line(photo_bgr, ap, bp, (255, 255, 255), 5)
+            points2D, _ = cv2.projectPoints(points3d, rotation_vector, translation_vector, camera_matrix, dist_coeffs)
 
-                ap = (int(points2D[0][0][0]), int(points2D[0][0][1]))
-                bp = (int(points2D[1][0][0]), int(points2D[1][0][1]))
-                cv2.line(photo_bgr, ap, bp, (255, 255, 255), 5)
+            ap = (int(points2D[3][0][0]), int(points2D[3][0][1]))
+            bp = (int(points2D[0][0][0]), int(points2D[0][0][1]))
+            cv2.line(photo_bgr, ap, bp, (255, 255, 255), 5)
 
-                ap = (int(points2D[1][0][0]), int(points2D[1][0][1]))
-                bp = (int(points2D[2][0][0]), int(points2D[2][0][1]))
-                cv2.line(photo_bgr, ap, bp, (255, 255, 255), 5)
+            ap = (int(points2D[0][0][0]), int(points2D[0][0][1]))
+            bp = (int(points2D[1][0][0]), int(points2D[1][0][1]))
+            cv2.line(photo_bgr, ap, bp, (255, 255, 255), 5)
 
-                ap = (int(points2D[2][0][0]), int(points2D[2][0][1]))
-                bp = (int(points2D[3][0][0]), int(points2D[3][0][1]))
-                cv2.line(photo_bgr, ap, bp, (255, 255, 255), 5)
+            ap = (int(points2D[1][0][0]), int(points2D[1][0][1]))
+            bp = (int(points2D[2][0][0]), int(points2D[2][0][1]))
+            cv2.line(photo_bgr, ap, bp, (255, 255, 255), 5)
+
+            ap = (int(points2D[2][0][0]), int(points2D[2][0][1]))
+            bp = (int(points2D[3][0][0]), int(points2D[3][0][1]))
+            cv2.line(photo_bgr, ap, bp, (255, 255, 255), 5)
+
         cv2.imshow('frame', photo_bgr)
 
         if input_type == '1':
